2b1a08bcda1ffbe500bac84dd3114dbcc4832a17
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-02-02 20:14:28 +0000

    Setup apple maps display

--- multi-map.js
+++ multi-map.js
@@ -14,6 +14,7 @@
 		 */
 		var settings = {};
 		var googleMap = {};
+		var appleMap = {};
 		var initCallback;
 
 		/**
@@ -28,9 +29,69 @@
 
 			// Load map API
 			if (settings.setup.platform == "google") {
-				var script = document.createElement("script");
-				script.src = "https://maps.googleapis.com/maps/api/js?key="+settings.google.apiKey+"&callback=MultiMap.setupGoogle";
-				document.head.appendChild(script);
+				_library.loadScript("https://maps.googleapis.com/maps/api/js?key="+settings.google.apiKey+"&callback=MultiMap.setupGoogle");
+			} else {
+				_library.loadScript("https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js", function() {
+					// Initialises MapKit JS using JWT authentication code
+					mapkit.init({
+						authorizationCallback: function(done) {
+							var xhr = new XMLHttpRequest();
+							xhr.open("GET", settings.apple.jwt);
+							xhr.addEventListener("load", function() {
+								done(this.responseText);
+							});
+							xhr.send();
+						}
+					});
+
+					_library.setupApple();
+				});
+
+			}
+		};
+
+		/**
+		 * Loads script into DOM
+		 * @param  {String}   path     [Path of script to be loaded]
+		 * @param  {Function} callback [Optional callback function]
+		 */
+		_library.loadScript = function(path, callback) {
+
+			var done = false;
+			var scr = document.createElement('script');
+
+			scr.onload = handleLoad;
+			scr.onreadystatechange = handleReadyStateChange;
+			scr.onerror = handleError;
+			scr.src = path;
+			document.body.appendChild(scr);
+
+			function handleLoad() {
+				if (!done) {
+					done = true;
+					if (typeof(callback) == "function") {
+						callback(path, "ok");
+					}
+				}
+			}
+
+			function handleReadyStateChange() {
+				var state;
+
+				if (!done) {
+					state = scr.readyState;
+					if (state === "complete") {
+						handleLoad();
+					}
+				}
+			}
+			function handleError() {
+				if (!done) {
+					done = true;
+					if (typeof(callback) == "function") {
+						callback(path, "error");
+					}
+				}
 			}
 		};
 
@@ -41,13 +102,33 @@
 		_library.setupGoogle = function() {
 			googleMap.loaded = true;
 
-			googleMap.map = new google.maps.Map(document.getElementById("map"), {
+			googleMap.map = new google.maps.Map(document.getElementById(setting.setup.map), {
 				center: {
 					lat: settings.setup.center.latitude, 
 					lng: settings.setup.center.longlitude
 				},
 				zoom: settings.google.zoom
 			});
+
+			if (typeof(initCallback) == "function") {
+				initCallback();
+			}
+		};
+
+		/**
+		 * Callback after MapKit JS API has loaded to setup map,
+		 * runs initCallback once map has been setup
+		 */
+		_library.setupApple = function() {
+			appleMap.loaded = true;
+
+	        var region = new mapkit.CoordinateRegion(
+	            new mapkit.Coordinate(settings.setup.center.latitude, settings.setup.center.longlitude),
+	            new mapkit.CoordinateSpan(settings.apple.coordinateSpan.latitude, settings.apple.coordinateSpan.longlitude)
+	        );
+
+	        appleMap.map = new mapkit.Map(settings.setup.map);
+	        appleMap.map.region = region;
 
 			if (typeof(initCallback) == "function") {
 				initCallback();
