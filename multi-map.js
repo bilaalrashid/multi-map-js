1fb56cd282bd8bfb4f66b246eaa0ff76ebf79d5b
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-02-02 19:30:20 +0000

    Added pins to google map

--- multi-map.js
+++ multi-map.js
@@ -3,10 +3,15 @@
 	function library() {
 		var _library = {};
 
-		var settings;
+		var settings = {};
+		var googleMap = {};
+		var initCallback;
 
-		_library.init = function(options) {
+
+		_library.init = function(options, callback) {
 			settings = options;
+
+			initCallback = callback;
 
 			_library.load();
 		};
@@ -14,16 +19,41 @@
 		_library.load = function() {
 			if (settings.setup.platform == "google") {
 				var script = document.createElement("script");
-				script.src = "https://maps.googleapis.com/maps/api/js?key="+settings.google.apiKey+"&callback=MultiMap.setup";
+				script.src = "https://maps.googleapis.com/maps/api/js?key="+settings.google.apiKey+"&callback=MultiMap.setupGoogle";
 				document.head.appendChild(script);
 			}
 		};
 
-		_library.setup = function() {
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: settings.setup.center.latitude, lng: settings.setup.center.longlitude},
+		_library.setupGoogle = function() {
+			googleMap.loaded = true;
+
+			googleMap.map = new google.maps.Map(document.getElementById("map"), {
+				center: {
+					lat: settings.setup.center.latitude, 
+					lng: settings.setup.center.longlitude
+				},
 				zoom: settings.google.zoom
 			});
+
+			if (typeof(initCallback) == "function") {
+				initCallback();
+			}
+		};
+
+		_library.addPin = function(position) {
+			if (settings.setup.platform == "google" && googleMap.loaded == true) {
+				var pin = new google.maps.Marker({
+					position: {
+						lat: position.latitude, 
+						lng: position.longlitude
+					}, 
+					map: googleMap.map
+				});
+
+				return pin;
+			}
+
+			return null;
 		};
 
 		return _library;
