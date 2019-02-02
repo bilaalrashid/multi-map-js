8c13915e315167325b4dc6787d3ea3ee7973623e
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-02-02 18:35:07 +0000

    Setup google maps display

--- multi-map.js
+++ multi-map.js
@@ -1,18 +1,36 @@
 (function(window){
 	// This function will contain all our code
 	function library() {
-		var _libraryObject = {};
+		var _library = {};
 
-		// Just create a property to our library object.
-		_libraryObject.init = function(prop) {
-			
+		var settings;
+
+		_library.init = function(options) {
+			settings = options;
+
+			_library.load();
 		};
 
-		return _libraryObject;
+		_library.load = function() {
+			if (settings.setup.platform == "google") {
+				var script = document.createElement("script");
+				script.src = "https://maps.googleapis.com/maps/api/js?key="+settings.google.apiKey+"&callback=MultiMap.setup";
+				document.head.appendChild(script);
+			}
+		};
+
+		_library.setup = function() {
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: {lat: settings.google.center.latitude, lng: settings.google.center.longlitude},
+				zoom: settings.google.zoom
+			});
+		};
+
+		return _library;
 	}
 
 	// We need that our library is globally accessible, then we save in the window
-	if(typeof(window.MultiMap) === "undefined") {
+	if (typeof(window.MultiMap) === "undefined") {
 		window.MultiMap = library();
 	}
 
