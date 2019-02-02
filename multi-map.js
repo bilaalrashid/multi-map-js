a12d7f117818cf0836bf2fee6f340284565d9876
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-02-02 19:43:47 +0000

    Added documentation to methods

--- multi-map.js
+++ multi-map.js
@@ -1,22 +1,26 @@
 (function(window){
-	// This function will contain all our code
+	// Library class function
 	function library() {
 		var _library = {};
 
+		/**
+		 * Instance variables
+		 */
 		var settings = {};
 		var googleMap = {};
 		var initCallback;
 
-
+		/**
+		 * Initialises the map
+		 * @param  {Object}   options  [Config options for map]
+		 * @param  {Function} callback [Optional callback after map has been setup]
+		 */
 		_library.init = function(options, callback) {
 			settings = options;
 
 			initCallback = callback;
 
-			_library.load();
-		};
-
-		_library.load = function() {
+			// Load map API
 			if (settings.setup.platform == "google") {
 				var script = document.createElement("script");
 				script.src = "https://maps.googleapis.com/maps/api/js?key="+settings.google.apiKey+"&callback=MultiMap.setupGoogle";
@@ -24,6 +28,10 @@
 			}
 		};
 
+		/**
+		 * Callback after Google Maps API has loaded to setup map,
+		 * runs initCallback once map has been setup
+		 */
 		_library.setupGoogle = function() {
 			googleMap.loaded = true;
 
@@ -40,6 +48,11 @@
 			}
 		};
 
+		/**
+		 * Adds pin to map
+		 * @param {Object} position [Position of pin to be added]
+		 * @return {?Pin} [Return pin]
+		 */
 		_library.addPin = function(position) {
 			if (settings.setup.platform == "google" && googleMap.loaded == true) {
 				var pin = new google.maps.Marker({
